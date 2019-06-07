967958f261959ce517f03005ebbaf9ebf2e45100
yinon <yinon.bentarya@gmail.com>
2019-06-07 09:55:05 +0300

    open close folders

--- folders-tree-app/src/app/components/folder/folder.component.ts
+++ folders-tree-app/src/app/components/folder/folder.component.ts
@@ -1,32 +1,72 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Folder } from 'src/app/interfaces/folder';
-import { FolderService } from 'src/app/services/folder.service';
-import { BehaviorSubject } from 'rxjs';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnDestroy
+} from "@angular/core";
+import { Folder } from "src/app/interfaces/folder";
+import { FolderService } from "src/app/services/folder.service";
+import { BehaviorSubject, Subject } from "rxjs";
+import "rxjs/add/operator/takeUntil";
 
 @Component({
-  selector: 'app-folder',
-  templateUrl: './folder.component.html',
-  styleUrls: ['./folder.component.scss']
+  selector: "app-folder",
+  templateUrl: "./folder.component.html",
+  styleUrls: ["./folder.component.scss"]
 })
-export class FolderComponent implements OnInit {
+export class FolderComponent implements OnInit, OnDestroy {
   @Input() folder: Folder;
   folderSelected$: BehaviorSubject<Folder>;
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  isOpen = false;
+  isSelected = false;
 
   constructor(private folderService: FolderService) {
     this.folderSelected$ = folderService.folderSelected$;
+    this.folderSelected$.takeUntil(this.destroy$).subscribe(folderSelected => {
+      this.setIsOpenIsSelected(folderSelected);
+    });
+  }
+
+  setIsOpenIsSelected(folderSelected: Folder) {
+    if (this.folder == null) {
+      return;
+    }
+    if (folderSelected != null) {
+      if (folderSelected.id === this.folder.id) {
+        // toggle current node selection
+        this.isSelected = !this.isSelected;
+        // if current node is not selected, close it
+        if (!this.isSelected) {
+          this.isOpen = false;
+        } else {
+          this.isOpen = true;
+        }
+      } else {
+        this.isSelected = false;
+        if (this.folder.folders.find(f => f.id === folderSelected.id)) {
+          // child node was selected, just stay open
+          this.isOpen = true;
+        }
+      }
+    } else {
+      // selection is cleared by clicking outside tree
+      this.isSelected = false;
+    }
   }
 
   ngOnInit() {}
 
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+
+    this.destroy$.unsubscribe();
+  }
+
   onClick(event: Event) {
     event.stopPropagation();
-    if (
-      this.folderSelected$.value &&
-      this.folderSelected$.value.id === this.folder.id
-    ) {
-      this.folderSelected$.next(null);
-    } else {
-      this.folderSelected$.next(this.folder);
-    }
+    this.folderSelected$.next(this.folder);
   }
 }
